3e0a98f895e3796599816a3aa7acbfa0ee3db0c3
mhesemans <micha_hesemans@hotmail.com>
2025-08-27 02:51:47 +0100

    Added checkout view, model and template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -60,6 +60,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'boutique_ado.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -76,6 +78,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
