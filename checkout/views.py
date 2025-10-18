82c4d96229a3d4526a80fcfafba078e1289f93fa
mhesemans <micha_hesemans@hotmail.com>
2025-10-18 05:19:21 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SJQqQK4fk4HNOrJI1O30JEd1xvh8pAOP4Hl5xRQqg3pRHYNJWCkB4yO4K1ABkytkZZzYf3blQx9P6G7DprmFoZM00Ovx0SmOG',
+        'client_secret': 'sk_test_51SJQqQK4fk4HNOrJhOEXCJKCxZ9429Cc8MK4SYzfr34oPYPWcl8rNxEPVqheMAiboBcoUh7csNy8dTXLbpItV7MC00tvrNrppG',
     }
 
     return render(request, template, context)
